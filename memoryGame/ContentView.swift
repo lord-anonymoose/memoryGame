a937bf9e8ef38f626656281638d5a68337a1fdb5
Philipp <philipp@MacBook-Air-Philipp.local>
2020-12-07 22:23:27 +0300

    Restructured some code

--- memoryGame/ContentView.swift
+++ memoryGame/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
-struct Card {
-    var value: String
+/*struct Card (id: Int) do {
+    var body: some View {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: String
+        Rectangle()
+        .fill(Color("cardColor"))
+        .frame(width:75, height: 75)
+    }
+}*/
+
+struct Card: View {
+    var content: String
+    var isTapped: Bool = false
+    var isMatched: Bool = false
+    
+    func tap() {
+        
+    }
+    
+    func matched() {
+        self.body.foregroundColor(.white)
+    }
+    
+    var body: some View {
+        Text(content)
+    }
+}
+
+struct memoryGame: View {
+    var chosenCards: Int = 0
+    var body: some View {
+        Card(content: "🎅🏻")
+    }
 }
 
 struct ContentView: View {
     var body: some View {
         Color("backgroundColor").edgesIgnoringSafeArea(.all)
             .overlay(
-                Text("Hello, world!")
+                memoryGame()
             )
     }
 }
